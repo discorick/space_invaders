df30c080abbe876d9a04de1537b869024af4b124
Richard Frank <richardfrank@me.com>
2013-03-07 11:20:13 -0700

    Save

--- lib/core/engine/space_engine.rb
+++ lib/core/engine/space_engine.rb
@@ -5,7 +5,7 @@ class SpaceEngine
   def initialize
     @arena = Arena.new
     @player_ship = Dispatcher.dispatch(SpaceShip.new){ :player_ship }
-    @invader_army = Dispatcher.dispatch(InvaderArmy.new)
+    @invader_army = Dispatcher.dispatch(InvaderArmy.new) #add_invaders
   end
 
 end
